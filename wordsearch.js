81d9a0d5ef9c08847845be36f93bff1817ae4d75
ive-m <ivesita.maria@gmail.com>
2022-10-19 14:48:56 -0400

    Paige-Ivette pair programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,35 @@
-const wordSearch = (letters, word) => { 
+
+const wordSearch = function (letters, word){
+if (wordSearchHorizontal(letters, word)===false) {
+    const transposed =transpose(letters);
+    return wordSearchHorizontal(transposed, word);
+}
+return true;
+
+};
+
+const wordSearchHorizontal = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
+    } 
+    
+
+    return false;
+};
+
+const transpose = function(matrix) {
+    let result = [];
+  
+    for (const i in matrix[0]) {
+      let column = [];
+      for (const x in matrix) {
+        column.push(matrix[x][i]);
+      }
+      result.push(column);
     }
-}
+    return result;
+  };
+
 
 module.exports = wordSearch
